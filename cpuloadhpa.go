4aa0fca95c6c38b0936bc6b3b8dfac371c3e9c0f
keyixie(谢可毅) <keyixie@tencent.com>
2021-10-14 14:23:38 +0800

    update

--- cpuloadhpa.go
+++ cpuloadhpa.go
@@ -1,6 +1,7 @@
 package cpuloadhpa
 
 import (
+	"container/list"
 	"context"
 	"runtime"
 	"time"
@@ -10,24 +11,29 @@ import (
 )
 
 type cl struct {
-	sleepTime     time.Duration
 	targetPercent int
 	ctx           context.Context
 	goroutineNums int
+
+	latestCpuPercent float64
+	workCancels      *list.List
 }
 type Options func(*cl)
 
 func WithGoroutineNums(num int) Options {
 	return func(c *cl) {
+		if num <= 0 {
+			num = 1
+		}
 		c.goroutineNums = num
 	}
 }
 func NewPayloadPercent(ctx context.Context, percent int, options ...Options) *cl {
 	res := &cl{
-		sleepTime:     time.Millisecond,
 		targetPercent: percent,
 		ctx:           ctx,
 		goroutineNums: runtime.GOMAXPROCS(0) / 2,
+		workCancels:   list.New().Init(),
 	}
 	for _, o := range options {
 		o(res)
@@ -37,51 +43,66 @@ func NewPayloadPercent(ctx context.Context, percent int, options ...Options) *cl
 }
 
 func (c *cl) UpdateTarget(target int) {
-	logrus.Infof("update target cpu: %v", c.targetPercent)
+	logrus.Infof("update target cpu: %v -> %v", c.targetPercent, target)
 	c.targetPercent = target
 }
 func (c *cl) GetTarget() int {
 	return c.targetPercent
 }
-func (c *cl) monitorSleepTime() {
-	l, r := time.Nanosecond, time.Second*10
+func (c *cl) monitorCPU() {
 	for {
-		select {
-		case <-c.ctx.Done():
-			return
-		default:
-			cpus, err := cpu.Percent(time.Millisecond*1215, false)
-			if err != nil {
-				logrus.Errorf("Err:%v", err)
-			}
-			percent := cpus[0]
-			mid := (l + r) / 2
-			c.sleepTime = mid
-			//logrus.Infof("sleep time : %v", c.sleepTime)
-			if percent > float64(c.targetPercent) {
-				l = mid
-			} else if percent <= float64(c.targetPercent) {
-				r = mid
-			}
+		cpus, err := cpu.Percent(time.Second, false)
+		if err != nil {
+			logrus.Errorf("Err:%v", err)
 		}
+		percent := cpus[0]
+		c.latestCpuPercent = percent
 
 	}
 }
 
-func (c *cl) AsyncRun() {
-	go c.monitorSleepTime()
+func (c *cl) GetLatestCPUPercent() float64 {
+	return c.latestCpuPercent
+}
+func (c *cl) GetWokers() int {
+	return c.workCancels.Len()
 
+}
+func (c *cl) AsyncRun() {
+	go c.monitorCPU()
 	for i := 0; i < c.goroutineNums; i++ {
-		go func() {
-			for {
-				select {
-				case <-c.ctx.Done():
-					return
-				default:
-					time.Sleep(c.sleepTime)
+		c.runNewWorker()
+	}
+}
+
+func (c *cl) killOneWorker() {
+
+}
+func (c *cl) runNewWorker() {
+	logrus.Infof("new worker added.")
+
+	go func() {
+		runtime.LockOSThread()
+		for {
+			select {
+			case <-c.ctx.Done():
+				return
+			default:
+				for {
+					unitHundresOfMicrosecond := 1000
+					runMicrosecond := unitHundresOfMicrosecond * c.targetPercent
+					sleepMicrosecond := unitHundresOfMicrosecond*100 - runMicrosecond
+					begin := time.Now()
+					for {
+						if time.Now().Sub(begin) > time.Duration(runMicrosecond)*time.Microsecond {
+							break
+						}
+					}
+					// sleep
+					time.Sleep(time.Duration(sleepMicrosecond) * time.Microsecond)
 				}
 			}
-		}()
-	}
+		}
+	}()
 
 }
